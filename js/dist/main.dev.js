9fcc2706afdc7d0d27ee7a6aaed69a7dbddbdb3b
Alexey Pavlov <pavlov.aleksey.m@gmail.com>
2020-11-28 20:40:39 +0300

    small fixes

--- js/dist/main.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var menuToggle = document.querySelector('#menu-toggle');
-var mobileNavContainer = document.querySelector('#mobile-nav');
-
-menuToggle.onclick = function () {
-  menuToggle.classList.toggle('menu-icon-active');
-  mobileNavContainer.classList.toggle('mobile-nav--active');
-};
